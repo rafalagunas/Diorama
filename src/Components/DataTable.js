2915a0d64523ad4c08824a670344443ef8757cad
rafastaria <guitronleafar@gmail.com>
2018-08-27 20:42:23 -0500

    upadted

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -1,85 +1,26 @@
 import React, { Component } from "react";
 // with es6
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+
+import {Container} from '../Styles/Table'
 //var Data = require("../Data/Firmes.js");
-var items = [
-  {
-    id: 75950,
-    rfc: "Louella Wallace",
-    razon_social: 24,
-    tipo_persona: "+44 (0)203 437 7302",
-    supuesto: "green"
-  },
-  {
-    id: 80616,
-    rfc: "Hanson Perry",
-    razon_social: 36,
-    tipo_persona: "+44 (0)203 279 3708",
-    supuesto: "brown"
-  },
-  {
-    id: 77621,
-    rfc: "Brandi Long",
-    razon_social: 20,
-    tipo_persona: "+44 (0)203 319 4880",
-    supuesto: "gray"
-  },
-  {
-    id: 81299,
-    rfc: "Tonia Sykes",
-    razon_social: 38,
-    tipo_persona: "+44 (0)208 328 3671",
-    supuesto: "blue"
-  },
-  {
-    id: 14225,
-    rfc: "Leach Durham",
-    razon_social: 23,
-    tipo_persona: "+44 (0)208 280 9572",
-    supuesto: "green"
-  },
-  {
-    id: 77621,
-    rfc: "Brandi Long",
-    razon_social: 20,
-    tipo_persona: "+44 (0)203 319 4880",
-    supuesto: "gray"
-  },
-  {
-    id: 81299,
-    rfc: "Tonia Sykes",
-    razon_social: 38,
-    tipo_persona: "+44 (0)208 328 3671",
-    supuesto: "blue"
-  },
-  {
-    id: 14225,
-    rfc: "Leach Durham",
-    razon_social: 23,
-    tipo_persona: "+44 (0)208 280 9572",
-    supuesto: "green"
-  },
-  {
-    id: 77621,
-    rfc: "Brandi Long",
-    razon_social: 20,
-    tipo_persona: "+44 (0)203 319 4880",
-    supuesto: "gray"
-  },
-  {
-    id: 81299,
-    rfc: "Tonia Sykes",
-    razon_social: 38,
-    tipo_persona: "+44 (0)208 328 3671",
-    supuesto: "blue"
-  },
-  {
-    id: 14225,
-    rfc: "Leach Durham",
-    razon_social: 23,
-    tipo_persona: "+44 (0)208 280 9572",
-    supuesto: "green"
-  }
+ var items = [
+{"id":0, "rfc": "&BC040317UN0", "razon_social": "E & B CONSTRUCTORES Y SUPERVISORES SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/12/2017"},
+{"id":1, "rfc": "&CA060106UG6", "razon_social": "A & C AUDITORIA Y CONTROL SC", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/4/2017"},
+{"id":2, "rfc": "&CT031008591", "razon_social": "A & C TRANSPORTE DE MAQUINARIA SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/9/2017"},
+{"id":3, "rfc": "&FP030911BI3", "razon_social": "E. & F. PROVEEDORA DE INSUMOS Y SERVICIOS SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/5/2016"},
+{"id":4, "rfc": "&GP020128G89", "razon_social": "E & G PUBLIVISION SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/1/2016"},
+{"id":5, "rfc": "&HI0102165P2", "razon_social": "A & H INCENTIVE GROUP SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/11/2014"},
+{"id":6, "rfc": "&IN051111Q96", "razon_social": "A & E INGENIERIA SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/2/2016"},
+{"id":7, "rfc": "&ML040319NR4", "razon_social": "E & M LOGISTIC SERVICE  SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/9/2017"},
+{"id":8, "rfc": "&RC030324PJ5", "razon_social": "A & R CONSTRUCCIONES SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/2/2016"},
+{"id":9, "rfc": "&RD021121TV4", "razon_social": "A & R DOORS & WINDOWS SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/11/2015"},
+{"id":10, "rfc": "&VE9904268V2", "razon_social": "E & A VIDEO EXCURSIONES SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/12/2015"},
+{"id":11, "rfc": "&VS040628LW0", "razon_social": "A & V SOLUCIONES SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/12/2017"},
+{"id":12, "rfc": "A&A020110E86", "razon_social": "ALDEN & ASSOCIATES SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "16/2/2015"},
+{"id":13, "rfc": "A&A0401096P2", "razon_social": "AF & ASSOCIATES THE DUBBING HOUSE SC", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/4/2017"},
+{"id":14, "rfc": "A&B050105G27", "razon_social": "AF & B MERCHANTS MEXICO SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/11/2016"},
+{"id":15, "rfc": "A&C050211RF6", "razon_social": "AUTOMATION & CONTROL ENGINEERING SYSTEMS SA DE CV", "tipo_persona": "M", "supuesto": "FIRMES","Fecha_pp": "1/1/2016"},
 ];
 
 class DataTable extends React.Component {
@@ -91,24 +32,28 @@ class DataTable extends React.Component {
 
   render() {
     return (
-      <div class="table-responsive">
+      <Container class="table-responsive">
         <BootstrapTable
-          class="table table-hover"
+          class="table-hover"
           data={items}
           version="4"
           pagination
           options={this.options}
+          headerStyle={ { background: '#f2f2f2' } }
+            bodyStyle={ { fontSize:13}  }
+            search
+            searchPlaceholder='Haz una búsqueda...' 
         >
           <TableHeaderColumn isKey dataField="id">
             Product ID
           </TableHeaderColumn>
           <TableHeaderColumn dataField="rfc">Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="razon_social">razon</TableHeaderColumn>
+          <TableHeaderColumn dataField="razon_social">razón social</TableHeaderColumn>
           <TableHeaderColumn dataField="tipo_persona">tipo</TableHeaderColumn>
           <TableHeaderColumn dataField="supuesto">supuesto</TableHeaderColumn>
           <TableHeaderColumn dataField="Fecha_pp">Fecha</TableHeaderColumn>
         </BootstrapTable>
-      </div>
+      </Container>
     );
   }
 }
---
+++ src/Styles/Table/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+font-size: 5px;
+.react-bs-table-container .table-responsive .react-bs-table .react-bs-table-bordered{
+    overflow-x:hidden;
+    
+}
+`;
