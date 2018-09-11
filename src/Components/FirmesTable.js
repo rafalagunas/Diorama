f32f4e91fa7f3f5bd875589f3a2fc16e20943ce8
rafastaria <guitronleafar@gmail.com>
2018-09-11 18:49:48 -0500

    updated

--- src/Components/FirmesTable.js
+++ src/Components/FirmesTable.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 // with es6
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
-
+import moment from "moment";
 import { Container } from "../Styles/Table";
 
-class FirmesTable extends React.Component {
+import auth from "./auth";
+
+export default class extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      items: [],
+      page: 1,
+      totalSize: 10,
+      fetchInfo: {
+        dataTotalSize: 10
+      }
+    };
 
-    this.options = {};
+    this.options = {
+      onPageChange: this.handlePaginator,
+      sizePerPage: 10
+    };
   }
 
+  componentDidMount() {
+    this.loadData();
+  }
+
+  handlePaginator = (page, sizePerPage) => {
+    this.setState(
+      {
+        page
+      },
+      this.loadData
+    );
+  };
+
+  loadData = () => {
+    const { page } = this.state,
+      offset = (page - 1) * 10,
+      query = `{ hub(id: ${auth.getHubId()}) {numberOfEntities entities (selector: "{\\"limit\\": 10, \\"offset\\": ${offset}}"){ id name date type }}}`;
+    auth.getData(query).then(res => {
+      this.setState({
+        items: res.data.hub.entities,
+        fetchInfo: {
+          dataTotalSize: res.data.hub.numberOfEntities
+        }
+      });
+    });
+  };
+
   render() {
+    const { items, fetchInfo } = this.state;
+
+    console.log(fetchInfo);
+
     return (
       <Container responsive>
         <BootstrapTable
@@ -20,6 +64,8 @@ class FirmesTable extends React.Component {
           version="4"
           hover
           pagination
+          remote={true}
+          fetchInfo={fetchInfo}
           options={this.options}
           headerStyle={{ background: "#f2f2f2" }}
           bodyStyle={{ fontSize: 12 }}
@@ -29,39 +75,23 @@ class FirmesTable extends React.Component {
           searchPlaceholder="Haz una búsqueda..."
         >
           <TableHeaderColumn isKey dataField="id" width="40px">
-            Id
-          </TableHeaderColumn>
-          <TableHeaderColumn width="130px" dataAlign="center" dataField="rfc">
             RFC
           </TableHeaderColumn>
-          <TableHeaderColumn width="280px" dataField="razon_social">
+          <TableHeaderColumn width="280px" dataField="name">
             Razón Social
           </TableHeaderColumn>
           <TableHeaderColumn
-            width="50px"
-            dataAlign="center"
-            dataField="tipo_persona"
-          >
-            Tipo
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            width="80px"
-            dataAlign="center"
-            dataField="supuesto"
-          >
-            Supuesto
-          </TableHeaderColumn>
-          <TableHeaderColumn
-            width="100px"
-            dataAlign="center"
-            dataField="Fecha_pp"
+            width="280px"
+            dataFormat={(cell, row) => moment(cell).format("YYYY-MM-DD")}
+            dataField="date"
           >
             Fecha
+          </TableHeaderColumn>
+          <TableHeaderColumn width="280px" dataField="type">
+            Tipo
           </TableHeaderColumn>
         </BootstrapTable>
       </Container>
     );
   }
 }
-
-export default FirmesTable;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Welcome from "./Welcome";
-
+import FirmesTable from "./FirmesTable";
 import { Title, TitleDiv } from "../Styles/Header";
 
 class App extends Component {
@@ -12,7 +12,8 @@ class App extends Component {
         <Welcome />
         <TitleDiv>
           <Title> FIRMES SAT</Title>
-        </TitleDiv>
+        </TitleDiv>{" "}
+        <FirmesTable />
         <TitleDiv>
           <Title> Animal Político</Title>
         </TitleDiv>
