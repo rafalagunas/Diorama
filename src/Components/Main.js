b5f63e8a70aa8d618cbc9e31b3f325f3c2f1f49d
rafastaria <guitronleafar@gmail.com>
2018-09-11 17:11:39 -0500

    updated

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Welcome from "./Welcome";
-import TableDemo from "./Table";
 import FirmesTable from "./FirmesTable";
-import ReactChartkick, { LineChart, PieChart } from "react-chartkick";
-import Chart from "chart.js";
 import { Title, TitleDiv } from "../Styles/Header";
 import AnimalPoliticoTable from "./AnimalPolitico";
 import Both from "./Both";
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Jumbotron, Container } from "reactstrap";
-import PropTypes from "prop-types";
 const Welcome = props => {
   return (
     <div>
