18dd73e4af90521ee2a5223113d64fc583af6693
M.O. Juan Manuel Ruiz Aranda <jmruiz.dev@gmail.com>
2018-09-11 17:23:31 -0500

    Mecate Team Update
    - Add src/Data/auth.js to connect with backend.
    - Add src/Components/DataHubClient.js to test connection to backend.
    - Update src/Components/App.js: add DataHubClient.js component.
    - Update .gitignore

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,6 +5,26 @@ import FirmesTable from "./FirmesTable";
 import { Title, TitleDiv } from "../Styles/Header";
 import AnimalPoliticoTable from "./AnimalPolitico";
 import Both from "./Both";
+import DataHubClient from "./DataHubClient";
+
+var items = [
+  "1/12/2017",
+  "1/4/2017",
+  "1/9/2017",
+  "1/5/2016",
+  "1/1/2016",
+  "16/11/2014",
+  "16/2/2016",
+  "16/9/2017",
+  "16/2/2016",
+  "16/11/2015",
+  "16/12/2015",
+  "1/12/2017",
+  "16/2/2015",
+  "1/4/2017",
+  "1/11/2016",
+  "1/1/2016"
+];
 
 class App extends Component {
   render() {
@@ -24,6 +44,10 @@ class App extends Component {
           <Title> FIRMES SAT y Animal Político</Title>
         </TitleDiv>
         <Both />
+        <TitleDiv>
+          <Title>DataHubClient</Title>
+        </TitleDiv>
+        <DataHubClient />
       </div>
     );
   }
