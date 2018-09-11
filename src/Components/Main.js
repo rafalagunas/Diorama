8bc8e03d492914aa72ed8ef923245995ed4c2cbe
rafastaria <guitronleafar@gmail.com>
2018-09-11 17:10:18 -0500

    updated

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,34 +8,7 @@ import Chart from "chart.js";
 import { Title, TitleDiv } from "../Styles/Header";
 import AnimalPoliticoTable from "./AnimalPolitico";
 import Both from "./Both";
-var items = [
-  "1/12/2017",
-  "1/4/2017",
-  "1/9/2017",
-  "1/5/2016",
-  "1/1/2016",
-  "16/11/2014",
-  "16/2/2016",
-  "16/9/2017",
-  "16/2/2016",
-  "16/11/2015",
-  "16/12/2015",
-  "1/12/2017",
-  "16/2/2015",
-  "1/4/2017",
-  "1/11/2016",
-  "1/1/2016"
-];
 
-/*      <LineChart
-          data={{
-            "2017-05-13": 2,
-            "2017-05-20 ": 5,
-            "2017-05-15": 6,
-            "2017-05-30": 7,
-            "2017-05-17": 8
-          }}
-        />*/
 class App extends Component {
   render() {
     return (
